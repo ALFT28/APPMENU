ae3d7033cbfcdbbc135b70190ac37854dd37b13b
JOEL <JOEL@DESKTOP-HEVUQ5C>
2024-06-12 21:13:43 -0600

    Agregar archivos de proyecto.

---
+++ APPMENU/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace APPMENU
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ APPMENU/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace APPMENU
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
